a64311a4ef7366ec79dc7a5c48498e5a41284bae
Pharmman <pharm.sale777@gmail.com>
2021-06-01 15:14:58 +0300

    refactoring

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './App.css'
-import {BrowserRouter} from 'react-router-dom'
 import {Provider} from 'react-redux'
 import {store} from './store'
 import {Main} from './main/Main'
@@ -8,11 +7,9 @@ import {Main} from './main/Main'
 function App() {
     return (
         <div className="App">
-            <BrowserRouter>
                 <Provider store={store}>
                     <Main/>
                 </Provider>
-            </BrowserRouter>
         </div>
     )
 }
